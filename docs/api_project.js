bbbfee28e7e2ecd365e9014bead42ae39218067c
baptiste <baptiste@bmagic.fr>
2017-01-26 20:27:29 +0100

    Add documentation

--- docs/api_project.js
+++ docs/api_project.js
@@ -7,7 +7,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2017-01-25T22:28:19.035Z",
+    "time": "2017-01-26T19:26:52.483Z",
     "url": "http://apidocjs.com",
     "version": "0.17.4"
   }
