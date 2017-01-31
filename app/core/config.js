e95c25f248672d0c93054b13f12a8549a6b681a4
baptiste <baptiste@bmagic.fr>
2017-01-31 22:32:59 +0100

    Change config file

--- app/core/config.js
+++ app/core/config.js
@@ -5,7 +5,10 @@ module.exports.load = function (callback) {
 
     //Load config file
     applicationStorage.env = process.env.NODE_ENV || "dev";
-    applicationStorage.config = require("config/config." + applicationStorage.env + ".js");
+    if (applicationStorage.env == "dev")
+        applicationStorage.config = require("config/config." + applicationStorage.env + ".js");
+    else
+        applicationStorage.config = require("config/config.js");
 
     callback();
 };
